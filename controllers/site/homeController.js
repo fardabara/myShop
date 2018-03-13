6b8078b88befec63e7450177a2176e02e7c7434c
fardabara <farhad_mohammadali@yahoo.com>
2018-03-13 23:31:34 +0330

    added icon basket and mock products in home page

--- controllers/site/homeController.js
+++ controllers/site/homeController.js
@@ -1,5 +1,28 @@
 let _route = 'site/home/';
 
 exports.index = (req, res) => {
-  res.render(_route + 'index');
+  let products = [{
+    image: 'http://via.placeholder.com/300x300',
+    thumbnail: 'Thumbnail label',
+    description: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+  }, {
+    image: 'http://via.placeholder.com/300x300',
+    thumbnail: 'Thumbnail label',
+    description: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+  }, {
+    image: 'http://via.placeholder.com/300x300',
+    thumbnail: 'Thumbnail label',
+    description: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+  }, {
+    image: 'http://via.placeholder.com/300x300',
+    thumbnail: 'Thumbnail label',
+    description: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+  }, {
+    image: 'http://via.placeholder.com/300x300',
+    thumbnail: 'Thumbnail label',
+    description: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+  }];
+  res.render(_route + 'index', {
+    products: products
+  });
 };
